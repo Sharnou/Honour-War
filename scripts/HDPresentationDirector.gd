ccac5e62941ad9fe8147d18abde00c0352242357
FOX <Ahmed_sharnou@yahoo.com>
2026-09-09 00:23:20 +0300

    Fix HD presentation director runtime binding

--- scripts/HDPresentationDirector.gd
+++ scripts/HDPresentationDirector.gd
@@ -11,16 +11,26 @@ extends Node3D
 var _camera: Camera3D
 var _target: Node3D
 
-func _ready() -> void:
-    _camera = get_node_or_null(camera_path) as Camera3D
-    _target = get_node_or_null(target_path) as Node3D
+func set_camera(value: Camera3D) -> void:
+    _camera = value
     if _camera:
         _camera.projection = Camera3D.PROJECTION_ORTHOGONAL
         _camera.size = orthographic_size
         _camera.current = true
 
+func set_target(value: Node3D) -> void:
+    _target = value
+
+func _ready() -> void:
+    var resolved_camera := get_node_or_null(camera_path) as Camera3D
+    if resolved_camera:
+        set_camera(resolved_camera)
+    var resolved_target := get_node_or_null(target_path) as Node3D
+    if resolved_target:
+        set_target(resolved_target)
+
 func _process(delta: float) -> void:
-    if not _camera or not _target:
+    if not _camera or not is_instance_valid(_camera) or not _target or not is_instance_valid(_target):
         return
     var desired := _target.global_position + Vector3(0.0, camera_height, camera_distance)
     _camera.global_position = _camera.global_position.lerp(desired, clamp(delta * camera_smoothing, 0.0, 1.0))
