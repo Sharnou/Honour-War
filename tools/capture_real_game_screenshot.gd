51f479f4bc021226337cd7a41cac544057a2edcf
FOX <Ahmed_sharnou@yahoo.com>
2026-09-19 00:04:57 +0300

    Add real game screenshot capture for visual QA

---
+++ tools/capture_real_game_screenshot.gd
@@ -0,0 +1,30 @@
+extends SceneTree
+
+const CAPTURE_DIR:String="res://visual-captures"
+
+func _initialize()->void:
+    get_root().set_meta("hw_visual_capture",true)
+    call_deferred("_launch")
+
+func _launch()->void:
+    var error:int=DirAccess.make_dir_recursive_absolute(ProjectSettings.globalize_path(CAPTURE_DIR))
+    if error!=OK and error!=ERR_ALREADY_EXISTS:
+        push_error("Could not create visual capture directory")
+        quit(1)
+        return
+    var result:Error=get_tree().change_scene_to_file("res://Main3D.tscn")
+    if result!=OK:
+        push_error("Could not launch Main3D.tscn for real-game capture")
+        quit(1)
+        return
+    await create_timer(7.0).timeout
+    await RenderingServer.frame_post_draw
+    var image:Image=get_viewport().get_texture().get_image()
+    var output:String=ProjectSettings.globalize_path(CAPTURE_DIR+"/honour-war-real-game.png")
+    var save_error:Error=image.save_png(output)
+    if save_error!=OK:
+        push_error("Real game screenshot save failed")
+        quit(1)
+        return
+    print("REAL_GAME_SCREENSHOT="+output)
+    quit(0)
