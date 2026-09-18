2bc00e42406380600980d00027fe616109a98823
FOX <Ahmed_sharnou@yahoo.com>
2026-09-18 13:57:32 +0300

    Set Fourth Job at Lv150 in class tree

--- scripts/ClassTreeSystem.gd
+++ scripts/ClassTreeSystem.gd
@@ -1,7 +1,7 @@
 class_name ClassTreeSystem
 extends RefCounted
 
-const TIER_LEVELS:Dictionary={1:1,2:25,3:50,4:100,5:200}
+const TIER_LEVELS:Dictionary={1:1,2:25,3:50,4:150,5:200}
 const TIER_NAMES:Dictionary={1:"Foundation",2:"Specialization",3:"Advanced",4:"Mastery",5:"Transcendence"}
 
 static func class_profile(class_id:String)->Dictionary:
