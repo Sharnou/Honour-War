68407453b8fdcc096a84094ddf5273549391c600
FOX <Ahmed_sharnou@yahoo.com>
2026-09-19 21:00:33 +0300

    Correct Main and Game3D preload paths

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 const GameDataClass=preload("res://scripts/GameData.gd")
-const SaveSystemClass=preload("res://scripts/SaveSystemClass.gd")
+const SaveSystemClass=preload("res://scripts/SaveSystem.gd")
 const PetSystemClass=preload("res://scripts/PetSystemClass.gd")
 const EquipmentSystemClass=preload("res://scripts/EquipmentSystemClass.gd")
 const LootSystemClass=preload("res://scripts/LootSystemClass.gd")
