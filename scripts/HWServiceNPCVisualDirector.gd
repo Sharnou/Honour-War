dea1397840a61f9241736c3f8348175bf3864a9e
FOX <Ahmed_sharnou@yahoo.com>
2026-09-19 15:25:20 +0300

    Correct explicit dependency preload paths

--- scripts/HWServiceNPCVisualDirector.gd
+++ scripts/HWServiceNPCVisualDirector.gd
@@ -1,6 +1,6 @@
 extends Node
 
-const TeleportSystemClass = preload("res://scripts/TeleportSystemClass.gd")3D
+const TeleportSystemClass = preload("res://scripts/TeleportSystem.gd")3D
 
 ## Town service NPC presentation. Fixed MMORPG service NPCs only; no player ownership/building.
 const Profiles=preload("res://scripts/HWWorldActorVisualProfiles.gd")
