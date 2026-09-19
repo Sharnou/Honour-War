d51884004d6cd2fa578968645895a3addf20bb19
FOX <Ahmed_sharnou@yahoo.com>
2026-09-19 20:17:56 +0300

    Correct explicit runtime dependency preload paths

--- scripts/WarpGates3D.gd
+++ scripts/WarpGates3D.gd
@@ -1,7 +1,7 @@
 class_name WarpGates3D
 extends Node3D
 
-const TeleportSystemClass = preload("res://scripts/TeleportSystemClass.gd")
+const TeleportSystemClass = preload("res://scripts/TeleportSystem.gd")
 const WARP_GATE_SCRIPT=preload("res://scripts/WarpGate3D.gd")
 const GATES:Array[Dictionary]=[
 	{"map_id":10,"name":"Prontera Sewer","color":Color("#56d6ff"),"pos":Vector3(6.5,0.05,10.5),"x":180,"y":450},
