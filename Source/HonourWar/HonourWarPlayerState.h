cd29844658b1967066f065454304e9e61b31c53b
FOX <Ahmed_sharnou@yahoo.com>
2026-09-21 23:34:23 +0300

    Add replicated guild membership and rank state

--- Source/HonourWar/HonourWarPlayerState.h
+++ Source/HonourWar/HonourWarPlayerState.h
@@ -14,9 +14,14 @@ public:
 
     UPROPERTY(Replicated, BlueprintReadOnly) int32 TeamId=0;
     UPROPERTY(Replicated, BlueprintReadOnly) int32 PartySlot=0;
+    UPROPERTY(Replicated, BlueprintReadOnly) FString GuildName;
+    UPROPERTY(Replicated, BlueprintReadOnly) FString GuildRank=TEXT("Member");
 
     int32 GetTeamId() const { return TeamId; }
     int32 GetPartySlot() const { return PartySlot; }
+    FString GetGuildName() const { return GuildName; }
+    FString GetGuildRank() const { return GuildRank; }
+    void SetGuild(const FString& Name,const FString& Rank);
 
 protected:
     virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
