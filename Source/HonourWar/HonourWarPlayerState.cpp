9d498e820e1fecc1df1edfd7b39abdbb0510fb9f
FOX <Ahmed_sharnou@yahoo.com>
2026-09-21 23:34:27 +0300

    Implement replicated guild membership state

--- Source/HonourWar/HonourWarPlayerState.cpp
+++ Source/HonourWar/HonourWarPlayerState.cpp
@@ -6,9 +6,17 @@ AHonourWarPlayerState::AHonourWarPlayerState()
     bReplicates=true;
 }
 
+void AHonourWarPlayerState::SetGuild(const FString& Name,const FString& Rank)
+{
+    GuildName=Name;
+    GuildRank=Rank;
+}
+
 void AHonourWarPlayerState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
 {
     Super::GetLifetimeReplicatedProps(OutLifetimeProps);
     DOREPLIFETIME(AHonourWarPlayerState,TeamId);
     DOREPLIFETIME(AHonourWarPlayerState,PartySlot);
+    DOREPLIFETIME(AHonourWarPlayerState,GuildName);
+    DOREPLIFETIME(AHonourWarPlayerState,GuildRank);
 }
