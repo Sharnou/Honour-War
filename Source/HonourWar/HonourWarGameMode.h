f48774ba887441900b23d2fd9fb2c3a42cc0060b
FOX <Ahmed_sharnou@yahoo.com>
2026-09-22 01:05:53 +0300

    Remove rejected team and 4v4 strategy admission hooks

--- Source/HonourWar/HonourWarGameMode.h
+++ Source/HonourWar/HonourWarGameMode.h
@@ -13,8 +13,6 @@ class HONOURWAR_API AHonourWarGameMode : public AGameModeBase
 public:
     AHonourWarGameMode();
     virtual void BeginPlay() override;
-    virtual void PostLogin(APlayerController* NewPlayer) override;
-    virtual void PreLogin(const FString& Options,const FString& Address,const FUniqueNetIdRepl& UniqueId,FString& ErrorMessage) override;
     bool HandleGuildCommand(AHonourWarCharacter* Character,const FString& Command,FString& OutMessage);
     bool HandleChatCommand(AHonourWarCharacter* Character,const FString& Message,FString& OutMessage);
 };
