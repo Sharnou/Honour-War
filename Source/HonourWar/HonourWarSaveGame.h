99947c768699f3eea874aac2407c3692dae4c233
FOX <Ahmed_sharnou@yahoo.com>
2026-09-21 19:49:33 +0300

    Persist exact player world location for resume

--- Source/HonourWar/HonourWarSaveGame.h
+++ Source/HonourWar/HonourWarSaveGame.h
@@ -20,6 +20,7 @@ public:
     UPROPERTY() EHonourWarFifthTierArchetype FifthTierArchetype = EHonourWarFifthTierArchetype::AbyssalWarlord;
     UPROPERTY() FDateTime SavedAtUtc;
     UPROPERTY() int64 OnlineSeconds = 0;
+    UPROPERTY() FVector PlayerLocation = FVector::ZeroVector;
     UPROPERTY() int64 Zeny = 0;
     UPROPERTY() int32 Honours = 0;
     UPROPERTY() TArray<FString> InventoryItems;
