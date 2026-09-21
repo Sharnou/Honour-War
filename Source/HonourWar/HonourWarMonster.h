c7f958ec8715e890d0f0a37ddef6967559960204
FOX <Ahmed_sharnou@yahoo.com>
2026-09-21 19:38:12 +0300

    Expose monster level for capped rewards

--- Source/HonourWar/HonourWarMonster.h
+++ Source/HonourWar/HonourWarMonster.h
@@ -16,6 +16,8 @@ public:
     AHonourWarMonster();
     virtual void Tick(float DeltaSeconds) override;
     void ReceiveCombatHit(float Damage, EHonourWarClass SourceClass);
+    void SetLevel(int32 NewLevel) { Level = FMath::Clamp(NewLevel, 1, 300); }
+    int32 GetMonsterLevel() const { return Level; }
     bool IsDead() const { return bDead; }
 
 protected:
