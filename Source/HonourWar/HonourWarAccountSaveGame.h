2936802faf992ba63a15c07b07a37aa3c5c7b65f
FOX <Ahmed_sharnou@yahoo.com>
2026-09-22 23:30:06 +0300

    Add persistent character roster slots for account selection

--- Source/HonourWar/HonourWarAccountSaveGame.h
+++ Source/HonourWar/HonourWarAccountSaveGame.h
@@ -2,7 +2,20 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/SaveGame.h"
+#include "HonourWarTypes.h"
 #include "HonourWarAccountSaveGame.generated.h"
+
+USTRUCT()
+struct FHonourWarCharacterSlot
+{
+    GENERATED_BODY()
+
+    UPROPERTY() bool bOwned=false;
+    UPROPERTY() FString CharacterName;
+    UPROPERTY() EHonourWarClass ClassId=EHonourWarClass::Warrior;
+    UPROPERTY() int32 Level=1;
+    UPROPERTY() int32 EquipmentRefineLevel=0;
+};
 
 UCLASS()
 class HONOURWAR_API UHonourWarAccountSaveGame : public USaveGame
@@ -14,4 +27,5 @@ public:
     UPROPERTY() FString PasswordHash;
     UPROPERTY() FDateTime CreatedAtUtc;
     UPROPERTY() FDateTime LastLoginAtUtc;
+    UPROPERTY() TArray<FHonourWarCharacterSlot> Characters;
 };
