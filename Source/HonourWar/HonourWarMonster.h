482ccbd1cba4d2fb7bed9293e69b02cc4ec5d0b7
FOX <Ahmed_sharnou@yahoo.com>
2026-09-23 00:24:19 +0300

    Add monster HIT FLEE LUK and flinch reaction state

--- Source/HonourWar/HonourWarMonster.h
+++ Source/HonourWar/HonourWarMonster.h
@@ -29,18 +29,23 @@ class HONOURWAR_API AHonourWarMonster : public AActor
 public:
     AHonourWarMonster();
     virtual void Tick(float DeltaSeconds) override;
-    void ReceiveCombatHit(float Damage, EHonourWarClass SourceClass);
+    void ReceiveCombatHit(float Damage, EHonourWarClass SourceClass, bool bCritical=false);
     void SetLevel(int32 NewLevel);
     void SetSpecies(EHonourWarMonsterSpecies NewSpecies);
     FString GetSpeciesName() const;
     int32 GetMonsterLevel() const { return Level; }
     bool IsDead() const { return bDead; }
+    int32 GetHitRating() const { return 80 + Level / 3; }
+    int32 GetFleeRating() const { return 50 + Level / 4; }
+    int32 GetCritResistance() const { return 2 + Level / 30; }
+    int32 GetLuck() const { return 8 + Level / 10; }
 
 protected:
     virtual void BeginPlay() override;
 
 private:
     void ApplySpeciesVisual();
+    void PlayCombatReaction(bool bHit,bool bCritical,bool bLucky,float Damage,EHonourWarClass SourceClass);
 
     UPROPERTY() USceneComponent* Root;
     UPROPERTY() UStaticMeshComponent* Body;
@@ -54,5 +59,7 @@ private:
     float MaxHealth = 800.0f;
     float CurrentHealth = 800.0f;
     float AttackTimer = 0.0f;
+    float FlinchTimer = 0.0f;
+    FVector ReactionBaseScale = FVector::OneVector;
     bool bDead = false;
 };
