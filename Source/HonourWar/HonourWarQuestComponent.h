6b5fdda7f0585229cf42252ae6e1003a68237257
FOX <Ahmed_sharnou@yahoo.com>
2026-09-23 17:46:01 +0300

    Expand Honour War quest progression to 12 objectives

--- Source/HonourWar/HonourWarQuestComponent.h
+++ Source/HonourWar/HonourWarQuestComponent.h
@@ -14,12 +14,12 @@ class HONOURWAR_API UHonourWarQuestComponent : public UActorComponent
 public:
     UHonourWarQuestComponent();
 
-    void RecordMonsterDefeat(int32 MonsterLevel);
+    void RecordMonsterDefeat(int32 MonsterLevel,const FString& MonsterSpecies);
     void SetQuestState(int32 InQuestId,int32 InProgress,bool bInComplete);
 
     int32 GetQuestId() const { return QuestId; }
     int32 GetQuestProgress() const { return QuestProgress; }
-    int32 GetQuestGoal() const { return QuestGoal; }
+    int32 GetQuestGoal() const;
     bool IsQuestComplete() const { return bQuestComplete; }
     FString GetQuestTitle() const;
     FString GetQuestBody() const;
@@ -28,9 +28,24 @@ protected:
     virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 
 private:
+    struct FQuestDefinition
+    {
+        int32 Id;
+        const TCHAR* Title;
+        const TCHAR* Body;
+        const TCHAR* RequiredSpecies;
+        int32 RequiredLevel;
+        int32 Goal;
+        int64 RewardZeny;
+        int32 RewardHonour;
+        const TCHAR* RewardItem;
+    };
+
+    static const FQuestDefinition& DefinitionFor(int32 InQuestId);
+    static constexpr int32 QuestCount=12;
+
     UPROPERTY() UHonourWarCombatComponent* CombatComponent=nullptr;
     UPROPERTY(Replicated) int32 QuestId=1;
     UPROPERTY(Replicated) int32 QuestProgress=0;
     UPROPERTY(Replicated) bool bQuestComplete=false;
-    static constexpr int32 QuestGoal=5;
 };
