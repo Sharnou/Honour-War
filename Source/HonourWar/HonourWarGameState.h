7898d0b3718fad66b1640f1599f9208060a70d9b
FOX <Ahmed_sharnou@yahoo.com>
2026-09-23 17:45:40 +0300

    Add live Honour War world event state

--- Source/HonourWar/HonourWarGameState.h
+++ Source/HonourWar/HonourWarGameState.h
@@ -12,12 +12,28 @@ class HONOURWAR_API AHonourWarGameState : public AGameStateBase
 public:
     AHonourWarGameState();
 
+    virtual void Tick(float DeltaSeconds) override;
+
     void AddWorldMessage(const FString& Message);
     const TArray<FString>& GetWorldMessages() const { return WorldMessages; }
+
+    int32 GetActiveWorldEventId() const { return ActiveWorldEventId; }
+    FString GetActiveWorldEventTitle() const;
+    FString GetActiveWorldEventBody() const;
+    int32 GetEventSecondsRemaining() const;
+    float GetWorldEventDamageMultiplier() const;
+    float GetWorldEventXpMultiplier() const;
+    float GetWorldEventZenyMultiplier() const;
+    float GetWorldEventHonourMultiplier() const;
+    float GetWorldEventSpRegenMultiplier() const;
 
 protected:
     virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 
 private:
     UPROPERTY(Replicated) TArray<FString> WorldMessages;
+    UPROPERTY(Replicated) int32 ActiveWorldEventId = 0;
+    UPROPERTY(Replicated) float WorldEventElapsed = 0.0f;
+
+    static constexpr float WorldEventDuration = 180.0f;
 };
