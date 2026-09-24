9a2ae06bc488ab7b194e038068d8a74a3aa7b757
FOX <Ahmed_sharnou@yahoo.com>
2026-09-24 12:17:32 +0300

    Add automated UI E2E test entry point

--- Source/HonourWar/HonourWarHUDWidget.h
+++ Source/HonourWar/HonourWarHUDWidget.h
@@ -22,6 +22,7 @@ class HONOURWAR_API UHonourWarHUDWidget : public UUserWidget
 protected:
     virtual void NativeConstruct() override;
     virtual void NativeTick(const FGeometry& MyGeometry,float InDeltaTime) override;
+    bool RunAutomatedE2ETest(FString& OutFailure);
 
 private:
     void BuildSurface();
