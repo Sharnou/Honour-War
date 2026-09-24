dd41c0afb2bbac39af34ec8fb4b3b845360332cc
FOX <Ahmed_sharnou@yahoo.com>
2026-09-24 13:18:52 +0300

    Add one-hour all-class runtime soak director

--- Source/HonourWar/HonourWarScreenshotDirector.h
+++ Source/HonourWar/HonourWarScreenshotDirector.h
@@ -19,10 +19,27 @@ private:
     void RequestCapture();
     void FinishCapture();
     void FailCapture(const FString& Reason);
+    void SetupSoakTest();
+    void RunSoakPhase();
+    void SpawnSoakMonster();
+    void FinishSoakTest(bool bSuccess);
+    void RecordSoak(const FString& Line);
 
     FTimerHandle SetupTimer;
     int32 CaptureAttempts=0;
     FTimerHandle CaptureTimer;
     FTimerHandle ExitTimer;
     TWeakObjectPtr<AHonourWarMonster> ShowcaseMonster;
+    TWeakObjectPtr<AHonourWarMonster> SoakMonster;
+    TArray<EHonourWarClass> SoakClasses;
+    int32 SoakClassIndex=0;
+    int32 SoakSkillIndex=0;
+    int32 SoakMovementRetries=0;
+    int32 SoakSkillSuccesses=0;
+    int32 SoakSkillFailures=0;
+    int32 SoakSaveCount=0;
+    float SoakWorldStartTime=0.0f;
+    float SoakLastReportTime=0.0f;
+    float SoakClassStartTime=0.0f;
+    FString SoakReport;
 };
